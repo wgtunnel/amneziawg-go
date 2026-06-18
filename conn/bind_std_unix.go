8b7434924b7582902067acb42d8b601b8e93c8b4
zaneschepke <dev@zaneschepke.com>
2026-06-18 03:57:45 -0400

    fix: register socket options to controlfns

--- conn/bind_std_unix.go
+++ conn/bind_std_unix.go
@@ -3,8 +3,25 @@
 package conn
 
 import (
+	"syscall"
+
 	"golang.org/x/sys/unix"
 )
+
+func init() {
+	// register socket options to controlFns
+	controlFns = append(controlFns, func(network, address string, c syscall.RawConn) error {
+		var opErr error
+		err := c.Control(func(fd uintptr) {
+			if e := setSocketOptions(fd); e != nil {
+			}
+		})
+		if err != nil {
+			return err
+		}
+		return opErr
+	})
+}
 
 func setSocketOptions(fd uintptr) error {
 	if err := unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, unix.SO_REUSEADDR, 1); err != nil {
